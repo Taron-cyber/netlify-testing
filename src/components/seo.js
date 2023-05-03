e2c7ae4657f0fad8ae3e107616c85853e4048e69
Taron <umroyan.447@gmail.com>
2023-05-03 18:09:07 +0400

    SEO Component & meta tags added

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,41 +5,109 @@
  * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react"
+import { Helmet } from "react-helmet"
 
-function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+const SEO = ({ title, description, lang, meta = [] }) => {
+  const metaDescription =
+    description || "The Top 20 Destinations to Visit in Europe"
+  const defaultTitle = "The Best Gifts for Tech Enthusiasts in 2023"
 
   return (
-    <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
-    </>
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title ? `${title} | ${defaultTitle}` : defaultTitle}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription,
+        },
+        {
+          property: "og:title",
+          content: title || defaultTitle,
+        },
+        {
+          property: "og:description",
+          content: metaDescription,
+          //Specifies a brief description of the content on the page.
+        },
+        {
+          property: "og:type",
+          content: "website",
+          // Specifies the type of content on the page, such as a website, article, or video.
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+        {
+          name: "twitter:title",
+          content: title || defaultTitle,
+        },
+        {
+          name: "twitter:description",
+          content: metaDescription,
+        },
+
+        {
+          property: "og:url",
+          content: "https://master--tiny-meringue-2fe479.netlify.app/",
+          // Specifies the canonical URL of the page.
+        },
+
+        {
+          property: "og:image",
+          content:
+            "https://master--tiny-meringue-2fe479.netlify.app/static/705185e5354a42a4570368356f29b7e5/dfaf6/example.avif",
+          // Specifies the URL of the main image on the page.
+        },
+        {
+          property: "og:image:width",
+          content: "1200",
+        },
+        {
+          property: "og:image:height",
+          content: "600",
+        },
+        {
+          property: "og:image:alt",
+          content: "text to speech",
+        },
+        {
+          property: "og:image:type",
+          content: "image/avif",
+        },
+        {
+          property: "og:site_name",
+          content: "master--tiny-meringue",
+          // Specifies the name of the website or app that the page belongs to.
+        },
+        {
+          property: "og:locale",
+          content: "en_US",
+          // Specifies the locale or language of the content on the page.
+        },
+        // {
+        //   property: "og:article:published_time",
+        //   content: "2023-05-01T00:00:00+00:00",
+        //   // Specifies the date and time that an article was published on the page.
+        // },
+
+        {
+          property: "telegram:title",
+          content: "Don't miss out on our exclusive sale - ends tomorrow",
+        },
+        {
+          property: "telegram:card",
+          content: "summary",
+        },
+
+        ...meta,
+      ]}
+    />
   )
 }
 
-export default Seo
+export default SEO
