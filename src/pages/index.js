395d8b6f0e5445be305eda24ab952e6ce6d743ef
Taron <umroyan.447@gmail.com>
2023-05-08 12:29:45 +0400

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import * as React from "react"
 import Container from "../components/container/Container"
 import Layout from "../components/layout"
-import * as styles from "../components/index.module.css"
+// import * as styles from "../components/index.module.css"
 
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-  { text: "Server Side Rendering", url: "using-ssr" },
-  { text: "Deferred Static Generation", url: "using-dsg" },
-]
+// const samplePageLinks = [
+//   {
+//     text: "Page 2",
+//     url: "page-2",
+//     badge: false,
+//     description:
+//       "A simple example of linking to another page within a Gatsby site",
+//   },
+//   { text: "TypeScript", url: "using-typescript" },
+//   { text: "Server Side Rendering", url: "using-ssr" },
+//   { text: "Deferred Static Generation", url: "using-dsg" },
+// ]
 
 const IndexPage = () => {
   return (
