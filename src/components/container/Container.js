b05ae1309bd9e1ad31d5908ec845b3a0bf2a9ba6
Taron <umroyan.447@gmail.com>
2023-05-10 16:06:04 +0400

    fix

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,35 +1,22 @@
 import * as React from "react"
-import Layout from "../layout"
 import { StaticImage } from "gatsby-plugin-image"
-import BackgroundImage from "gatsby-background-image"
+import Layout from "../layout"
 import Seo from "../seo"
+
 import "../container/ContainerStyle.scss"
 
 const Container = () => {
   return (
     <Layout>
       <Seo title="The Top 20 Destinations to Visit in Europe" />
-      {/* <BackgroundImage
-        className="parent-container"
-        Tag="section"
-        fluid={{
-          src: "https://images.unsplash.com/photo-1668068810651-bfd8a7ee02ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
-          sizes: "(max-width: 640px) 100vw, (max-width: 1024px) 768px, 1024px",
-        }}
-        style={{ height: "100vh" }}
-        alt="background img"
-        loading="lazy"
-      > */}
-
-      <div className="parent-container">
-        <div className="child-container">
+      <div className="outer-container">
+        <div className="inner-container">
           <StaticImage
             className="img"
             src="https://w.forfun.com/fetch/05/05eeb93a2e41734ecb6044146351f11e.jpeg?h=900&r=0.5"
             loading="lazy"
             quality={100}
             formats={["webp"]}
-            // style={{ marginBottom: `var(--space-3)` }}
             alt="img"
           />
           <h2>Some Content </h2>
@@ -45,7 +32,6 @@ const Container = () => {
           </div>
         </div>
       </div>
-      {/* </BackgroundImage> */}
     </Layout>
   )
 }
