237365717bb7e9ae6fd8d20170b0a5df59da2d6b
Taron <umroyan.447@gmail.com>
2023-05-18 17:29:32 +0400

    fix

--- src/sections/index/LetsGetStartedSection.js
+++ src/sections/index/LetsGetStartedSection.js
@@ -54,8 +54,8 @@ const LetsGetStartedSection = () => {
             Send request
           </button>
           <div className="checkbox-container">
-            <input type="checkbox" id="checkbox" name="checkbox" /> Subscribe to
-            our newsletters
+            <input type="checkbox" id="checkbox" name="checkbox" />
+            <label>Subscribe to our newsletters</label>
           </div>
         </div>
       </form>
